665985f7eac2036c5df2ea448c9b8b545b578b36
danielgarsden@outlook.com <danielgarsden@outlook.com>
2020-10-04 21:23:17 +0100

    Added Repository Interface

---
+++ SubjectOrderDetails/Services/ISubjectOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using SubjectOrderDetails.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubjectOrderDetails.Services
+{
+    public interface ISubjectOrderRepository
+    {
+
+        IEnumerable<Subject> GetSubjects();
+        Subject GetSubject(int subjectId);
+
+        void AddSubject(Subject subject);
+        void DeleteSubject(Subject subject);
+        void UpdateSubject(Subject subject);
+        bool SubjectExists(int subjectId);
+        bool Save();
+    }
+}
