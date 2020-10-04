fff5898f9a9d7539953399b366774a816551466a
danielgarsden@outlook.com <danielgarsden@outlook.com>
2020-10-04 21:10:36 +0100

    Add project files.

---
+++ SubjectOrderDetails/Entities/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubjectOrderDetails.Entities
+{
+    public class Subject
+    {
+
+        [Key]
+        public int subjectId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string firstName { get; set; }
+
+        [MaxLength(100)]
+        public string lastnameName { get; set; }
+
+    }
+}
