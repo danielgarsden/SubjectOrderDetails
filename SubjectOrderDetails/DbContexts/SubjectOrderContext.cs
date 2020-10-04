786093626c0fa26254636a1796857ca8315a529f
danielgarsden@outlook.com <danielgarsden@outlook.com>
2020-10-04 21:28:59 +0100

    Added DBContext

---
+++ SubjectOrderDetails/DbContexts/SubjectOrderContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SubjectOrderDetails.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SubjectOrderDetails.DbContexts
+{
+    public class SubjectOrderContext : DbContext
+    {
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
