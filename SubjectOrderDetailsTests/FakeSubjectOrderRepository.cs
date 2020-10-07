0b274aee689e859fca665f9d2ffc8e2bd17fe471
danielgarsden@outlook.com <danielgarsden@outlook.com>
2020-10-07 11:16:49 +0100

    Added Unit Test Project

---
+++ SubjectOrderDetailsTests/FakeSubjectOrderRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SubjectOrderDetails.Entities;
+using SubjectOrderDetails.Services;
+
+namespace SubjectOrderDetailsTests
+{
+    public class FakeSubjectOrderRepository : ISubjectOrderRepository
+    {
+        private readonly List<Subject> _subject;
+
+        public FakeSubjectOrderRepository()
+        {
+            _subject = new List<Subject>()
+            {
+                new Subject()
+                {
+                    subjectId = 1,
+                    firstName = "Daniel",
+                    lastName = "Garsden",
+                    dateOfBirth = new DateTime(1976, 8, 19),
+                    titleId = 1
+                },
+                new Subject()
+                {
+                    subjectId = 1,
+                    firstName = "Tamas",
+                    lastName = "Garsden",
+                    dateOfBirth = new DateTime(2015, 11, 29),
+                    titleId = 1
+                },
+                new Subject()
+                {
+                    subjectId = 1,
+                    firstName = "Alma",
+                    lastName = "Garsden",
+                    dateOfBirth = new DateTime(2017, 11, 16),
+                    titleId = 1
+                }
+            };
+        }
+
+        public void AddSubject(Subject subject)
+        {
+            subject.subjectId = 4;
+            _subject.Add(subject);
+        }
+
+        public void DeleteSubject(Subject subject)
+        {
+            _subject.Remove(subject);
+        }
+
+        public Subject GetSubject(int subjectId)
+        {
+            return _subject.Where(s => s.subjectId == subjectId).FirstOrDefault();
+        }
+
+        public IEnumerable<Subject> GetSubjects()
+        {
+            return _subject;
+        }
+
+        public IEnumerable<Title> GetTitles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SubjectExists(int subjectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSubject(Subject subject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SubjectOrderDetailsTests/SubjectsControllerTest.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SubjectOrderDetails.Controllers;
+using SubjectOrderDetails.Models;
+using System.Collections.Generic;
+
+namespace SubjectOrderDetailsTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetSubjects_ReturnsOkResult()
+        {
+            // arrange
+            FakeSubjectOrderRepository fake = new FakeSubjectOrderRepository();
+            SubjectsController sc = new SubjectsController(fake);
+
+            // act
+            var result = sc.GetSubjects();
+
+            // assert
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+        }
+
+        [Test]
+        public void GetSubjects_ReturnsSubjectDtoList()
+        {
+            // arrange
+            FakeSubjectOrderRepository fake = new FakeSubjectOrderRepository();
+            SubjectsController sc = new SubjectsController(fake);
+
+            // act
+            var result = sc.GetSubjects().Result as OkObjectResult;
+
+            // Assert
+            Assert.IsInstanceOf<List<SubjectDto>>(result.Value);
+        }
+
+
+        [Test]
+        public void GetSubjects_ReturnsAllItems()
+        {
+            // arrange
+            FakeSubjectOrderRepository fake = new FakeSubjectOrderRepository();
+            SubjectsController sc = new SubjectsController(fake);
+
+            // act
+            var result = sc.GetSubjects().Result as OkObjectResult;
+            List<SubjectDto> subjects =  result.Value as List<SubjectDto>;
+
+            // assert
+            Assert.AreEqual(3, subjects.Count);
+        }
+
+        [Test]
+        public void GetSubject_ValidSubject_ReturnsOkResult()
+        {
+            // arrange
+            FakeSubjectOrderRepository fake = new FakeSubjectOrderRepository();
+            SubjectsController sc = new SubjectsController(fake);
+
+            // act
+            var result = sc.GetSubject(1);
+
+            // assert
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+        }
+
+        [Test]
+        public void GetSubject_InValidSubject_ReturnsNotFoundResult()
+        {
+            // arrange
+            FakeSubjectOrderRepository fake = new FakeSubjectOrderRepository();
+            SubjectsController sc = new SubjectsController(fake);
+
+            // act
+            var result = sc.GetSubject(4);
+
+            // assert
+            Assert.IsInstanceOf<NotFoundResult>(result.Result);
+        }
+
+    }
+}
